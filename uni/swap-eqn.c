3172f00bcdb8778719f5c8b8424f9bac4ed03c7f
kamil-raja <kamilrk019@gmail.com>
2025-04-24 12:39:33 +0530

    Initial commit

---
+++ uni/swap-eqn.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    a = 10;
+    b = 20;
+    printf("before swap :a=%d , b = %d", a, b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("\nafter swap : a=%d, b=%d", a, b);
+    return 0;
+}
