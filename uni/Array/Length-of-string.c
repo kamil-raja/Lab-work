f83a425611b802922d4fe04d65c89af707626ff9
kamil-raja <kamilrk019@gmail.com>
2025-06-12 12:46:14 +0530

    add all

---
+++ uni/Array/Length-of-string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int length = 0;
+    printf("Enter a string : ");
+    fgets(str, sizeof(str), stdin);
+    // manually calculate length(excludig '\0')
+    while (str[length] != '\0')
+    {
+        if (str[length] == '\n')
+        {
+            break;
+        }
+        length++;
+    }
+    printf("Length of the string:%d\n ", length);
+    return 0;
+}
